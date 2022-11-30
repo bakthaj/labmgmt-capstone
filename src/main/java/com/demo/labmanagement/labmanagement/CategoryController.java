88aef931c407b59a6cab90c1a23fe0f443223cc9
bakthaj <palani160385@gmail.com>
2022-11-30 17:44:47 +0530

    fixes

--- src/main/java/com/demo/labmanagement/labmanagement/CategoryController.java
+++ src/main/java/com/demo/labmanagement/labmanagement/CategoryController.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+
 public class CategoryController {
     private final CategoryRepository categoryRepository;
 
--- src/main/java/com/demo/labmanagement/labmanagement/LabController.java
+++ src/main/java/com/demo/labmanagement/labmanagement/LabController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+
 public class LabController {
     private final LabRepository labRepository;
     private final CategoryRepository categoryRepository;
